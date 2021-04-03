55fab7f09525e08cb3848ff0a2c0b07efa79df32
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-04-03 03:07:57 -0400

    first commit

---
+++ src/welcome/HelloWorld.java
@@ -0,0 +1,16 @@
+package welcome;
+
+import java.util.HashMap;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, Integer> mp= new HashMap<String, Integer>();
+		mp.put("sam", 001);
+		mp.put("Dan", 005);
+		
+
+	}
+
+}
